34204e7aff6244de8c94b791b4b73454a3f21e23
sai7135 <credential.helper>
2024-01-09 15:15:59 -0500

    recipefront serve

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<recipe-front>'));
+app.use(express.static(__dirname + '/dist/recipe-front'));
 app.get('/*', function(req,res)
 
-{res.sendFile(path.join(__dirname+'/dist/<recipe-front>/index.html'));});
+{res.sendFile(path.join(__dirname+'/dist/recipe-front/index.html'));});
 
 app.listen(process.env.PORT || 8080);
